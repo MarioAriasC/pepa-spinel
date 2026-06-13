9da44f2bee39be66e430ae9161fa5742cee6b161
Mario Arias <mario@marioarias.org>
2026-06-14 02:14:25 +0800

    Evaluator

---
+++ lib/evaluator.rb
@@ -0,0 +1,355 @@
+# frozen_string_literal: true
+
+require_relative 'objects'
+module Evaluator
+  M_TRUE = Objects::MBoolean.new(true)
+  M_FALSE = Objects::MBoolean.new(false)
+
+  BUILTINS = {
+    Objects::LEN_NAME => Objects::LEN_BUILTIN,
+    Objects::PUSH_NAME => Objects::PUSH_BUILTIN,
+    Objects::FIRST_NAME => Objects::FIRST_BUILTIN,
+    Objects::LAST_NAME => Objects::LAST_BUILTIN,
+    Objects::REST_NAME => Objects::REST_BUILTIN
+  }.freeze
+
+  class Environment
+    def initialize(store = {}, outer = nil)
+      @store = store
+      @outer = outer
+    end
+
+    def []=(name, value)
+      @store[name] = value
+    end
+
+    def [](name)
+      obj = @store[name]
+      if obj.nil? && !@outer.nil?
+        @outer[name]
+      else
+        obj
+      end
+    end
+  end
+
+  def self.evaluate(program, environment)
+    result = nil
+    program.statements.each do |statement|
+      result = inner_eval(statement, environment)
+      case result
+      when Objects::MReturnValue
+        return result.value
+      when Objects::MError
+        return result
+      end
+    end
+    result
+  end
+
+  def self.inner_eval(node, environment)
+    case node
+    when Past::Identifier
+      eval_identifier(node, environment)
+    when Past::IntegerLiteral
+      Objects::MInteger.new(node.value)
+    when Past::InfixExpression
+      inner_eval(node.left, environment).if_not_error do |left|
+        inner_eval(node.right, environment).if_not_error do |right|
+          eval_infix_expression(node.operator, left, right)
+        end
+      end
+    when Past::BlockStatement
+      eval_block_statement(node, environment)
+    when Past::ExpressionStatement
+      inner_eval(node.expression, environment)
+    when Past::IfExpression
+      eval_if_expression(node, environment)
+    when Past::CallExpression
+      inner_eval(node.function, environment).if_not_error do |function|
+        args = eval_expressions(node.arguments, environment)
+        if args.size == 1 && args[0].error?
+          args[0]
+        else
+          apply_function(function, args)
+        end
+      end
+    when Past::ReturnStatement
+      inner_eval(node.return_value, environment).if_not_error do |value|
+        Objects::MReturnValue.new(value)
+      end
+    when Past::PrefixExpression
+      inner_eval(node.right, environment).if_not_error do |right|
+        eval_prefix_expression(node.operator, right)
+      end
+    when Past::BooleanLiteral
+      node.value.to_m
+    when Past::LetStatement
+      inner_eval(node.value, environment).if_not_error do |value|
+        environment[node.name.value] = value
+      end
+    when Past::FunctionLiteral
+      Objects::MFunction.new(node.parameters, node.body, environment)
+    when Past::StringLiteral
+      Objects::MString.new(node.value)
+    when Past::IndexExpression
+      left = inner_eval(node.left, environment)
+      return left if left.error?
+
+      index = inner_eval(node.index, environment)
+      return index if index.error?
+
+      eval_index_expression(left, index)
+    when Past::HashLiteral
+      eval_hash_literal(node, environment)
+    when Past::ArrayLiteral
+      elements = eval_expressions(node.elements, environment)
+      if elements.size == 1 && elements[0].error?
+        elements[0]
+      else
+        Objects::MArray.new(elements)
+      end
+    else
+      p node
+      nil
+    end
+  end
+
+  def self.eval_minus_prefix_operator_expression(right)
+    if right.nil?
+      nil
+    else
+      case right
+      when Objects::MInteger
+        -right
+      else
+        Objects::MError.new("unknown operator: -#{right.type_desc}")
+      end
+    end
+  end
+
+  def self.eval_index_expression(left, index)
+    if left.is_a?(Objects::MArray) && index.is_a?(Objects::MInteger)
+      eval_array_index_expression(left, index)
+    elsif left.is_a?(Objects::MHash)
+      eval_hash_index_expression(left, index)
+    else
+      Objects::MError.new("index operator not supported: #{left.type_desc}")
+    end
+  end
+
+  def self.eval_array_index_expression(array, index)
+    elements = array.elements
+    i = index.value
+    max = elements.size - 1
+    return Objects::M_NULL if i.negative? || i > max
+
+    elements[i]
+  end
+
+  def self.eval_hash_index_expression(hash, index)
+    case index
+    when Objects::MValue
+      pair = hash.pairs[index.hash_key]
+      if pair.nil?
+        Objects::M_NULL
+      else
+        pair.value
+      end
+    else
+      Objects::MError.new("unusable as a hash key: #{index.type_desc}")
+    end
+  end
+
+  def self.eval_hash_literal(hash_literal, environment)
+    pairs = {}
+    hash_literal.pairs.each do |key_node, value_node|
+      key = inner_eval(key_node, environment)
+      return key if key.error?
+
+      case key
+      when Objects::MValue
+        value = inner_eval(value_node, environment)
+        return value if value.error?
+
+        pairs[key.hash_key] = Objects::HashPair.new(key, value)
+      else
+        return Objects::MError.new("unusable as hash key: #{key.type_desc}")
+      end
+    end
+
+    Objects::MHash.new(pairs)
+  end
+
+  def self.eval_bang_operator_expression(right)
+    case right
+    when M_FALSE, Objects::M_NULL
+      M_TRUE
+    else
+      M_FALSE
+    end
+  end
+
+  def self.eval_expressions(arguments, environment)
+    arguments.map do |argument|
+      evaluated = inner_eval(argument, environment)
+      return [evaluated] if evaluated.error?
+
+      evaluated
+    end
+  end
+
+  def self.apply_function(function, args)
+    case function
+    when Objects::MFunction
+      extend_env = extend_function_env(function, args)
+      evaluated = inner_eval(function.body, extend_env)
+      unwrap_return_value(evaluated)
+    when Objects::MBuiltinFunction
+      result = function.fn.call(args)
+      if result.nil?
+        Objects::M_NULL
+      else
+        result
+      end
+    else
+      Objects::MError.new("not a function: #{function.type_desc}")
+    end
+  end
+
+  def self.extend_function_env(function, args)
+    env = Environment.new({}, function.environment)
+    function.parameters&.each_with_index do |identifier, i|
+      env[identifier.value] = args[i]
+    end
+    env
+  end
+
+  def self.unwrap_return_value(obj)
+    case obj
+    when Objects::MReturnValue
+      obj.value
+    else
+      obj
+    end
+  end
+
+  def self.eval_identifier(identifier, environment)
+    value = environment[identifier.value]
+    if value.nil?
+      builtin = BUILTINS[identifier.value]
+      if builtin.nil?
+        Objects::MError.new("identifier not found: #{identifier.value}")
+      else
+        builtin
+      end
+    else
+      value
+    end
+  end
+
+  def self.eval_prefix_expression(operator, right)
+    case operator
+    when '!'
+      eval_bang_operator_expression(right)
+    when '-'
+      eval_minus_prefix_operator_expression(right)
+    else
+      Objects::MError.new("Unknown operator : #{operator}#{right.type_desc}")
+    end
+  end
+
+  def self.eval_integer_infix_expression(operator, left, right)
+    case operator
+    when '+'
+      left + right
+    when '-'
+      left - right
+    when '*'
+      left * right
+    when '/'
+      left / right
+    when '<'
+      (left < right).to_m
+    when '>'
+      (left > right).to_m
+    when '=='
+      (left == right).to_m
+    when '!='
+      (left != right).to_m
+    else
+      Objects::MError.new("unknown operator: #{left.type_desc} #{operator} #{right.type_desc}")
+    end
+  end
+
+  def self.eval_infix_expression(operator, left, right)
+    if left.is_a?(Objects::MInteger) && right.is_a?(Objects::MInteger)
+      eval_integer_infix_expression(operator, left, right)
+    elsif operator == '=='
+      (left == right).to_m
+    elsif operator == '!='
+      (left != right).to_m
+    elsif left.type_desc != right.type_desc
+      Objects::MError.new("type mismatch: #{left.type_desc} #{operator} #{right.type_desc}")
+    elsif left.is_a?(Objects::MString) && right.is_a?(Objects::MString)
+      eval_string_infix_expression(operator, left, right)
+    else
+      Objects::MError.new("unknown operator: #{left.class} #{operator} #{right.class}")
+    end
+  end
+
+  def self.eval_string_infix_expression(operator, left, right)
+    if operator == '+'
+      left + right
+    else
+      Objects::MError.new("unknown operator: #{left.class} #{operator} #{right.class}")
+    end
+  end
+
+  def self.eval_if_expression(if_expression, environment)
+    inner_eval(if_expression.condition, environment).if_not_error do |condition|
+      if condition.truthy?
+        inner_eval(if_expression.consequence, environment)
+      elsif !if_expression.alternative.nil?
+        inner_eval(if_expression.alternative, environment)
+      else
+        Objects::M_NULL
+      end
+    end
+  end
+
+  def self.eval_block_statement(block_statement, environment)
+    result = nil
+    block_statement.statements.each do |statement|
+      result = inner_eval(statement, environment)
+      return result if result.is_a?(Objects::MReturnValue) || result.is_a?(Objects::MError)
+    end
+    result
+  end
+end
+
+class TrueClass
+  def to_m
+    Objects::MBoolean.new(true)
+  end
+end
+
+class FalseClass
+  def to_m
+    Objects::MBoolean.new(false)
+  end
+end
+
+class NilClass
+  def if_not_error(&)
+    self
+  end
+
+  def error?
+    false
+  end
+
+  def type_desc
+    'nil'
+  end
+end
---
+++ tests/evaluator_spec.rb
@@ -0,0 +1,384 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative 'spec_helper'
+require 'objects'
+describe 'Evaluator' do
+  it 'eval integer expressions' do
+    [
+      ['5', 5],
+      ['10', 10],
+      ['-5', -5],
+      ['-10', -10],
+      ['5 + 5 + 5 + 5 - 10', 10],
+      ['2 * 2 * 2 * 2 * 2', 32],
+      ['-50 + 100 + -50', 0],
+      ['5 * 2 + 10', 20],
+      ['5 + 2 * 10', 25],
+      ['20 + 2 * -10', 0],
+      ['50 / 2 * 2 + 10', 60],
+      ['2 * (5 + 10)', 30],
+      ['3 * 3 * 3 + 10', 37],
+      ['3 * (3 * 3) + 10', 37],
+      ['(5 + 10 * 2 + 15 / 3) * 2 + -10', 50]
+    ].each do |input, expected|
+      test_integer(input, expected)
+    end
+  end
+
+  it 'eval boolean expression' do
+    [
+      ['true', true],
+      ['false', false],
+      ['1 < 2', true],
+      ['1 > 2', false],
+      ['1 < 1', false],
+      ['1 > 1', false],
+      ['1 == 1', true],
+      ['1 != 1', false],
+      ['1 == 2', false],
+      ['1 != 2', true],
+      ['true == true', true],
+      ['false == false', true],
+      ['true == false', false],
+      ['true != false', true],
+      ['false != true', true],
+      ['(1 < 2) == true', true],
+      ['(1 < 2) == false', false],
+      ['(1 > 2) == true', false],
+      ['(1 > 2) == false', true]
+    ].each do |input, expected|
+      test_boolean(input, expected)
+    end
+  end
+
+  it 'bang operator' do
+    [
+      ['!true', false],
+      ['!false', true],
+      ['!5', false],
+      ['!!true', true],
+      ['!!false', false],
+      ['!!5', true]
+    ].each do |input, expected|
+      test_boolean(input, expected)
+    end
+  end
+
+  it 'if else expression' do
+    [
+      ['if (true) { 10 }', 10],
+      ['if (false) { 10 }', nil],
+      ['if (1) { 10 }', 10],
+      ['if (1 < 2) { 10 }', 10],
+      ['if (1 > 2) { 10 }', nil],
+      ['if (1 > 2) { 10 } else { 20 }', 20],
+      ['if (1 < 2) { 10 } else { 20 }', 10]
+    ].each do |input, expected|
+      evaluated = test_eval(input)
+      if expected.nil?
+        test_nil_object(evaluated)
+      else
+        test_integer_object(evaluated, expected)
+      end
+    end
+  end
+
+  it 'return statement' do
+    [
+      ['return 10;', 10],
+      ['return 10; 9;', 10],
+      ['return 2 * 5; 9;', 10],
+      ['9; return 2 * 5; 9;', 10],
+      ["if (10 > 1) {
+          if (10 > 1) {
+            return 10;
+          }
+
+          return 1;
+          }", 10],
+      ["let f = fn(x) {
+                return x;
+                x + 10;
+              };
+              f(10);", 10],
+      ["let f = fn(x) {
+                 let result = x + 10;
+                 return result;
+                 return 10;
+              };
+              f(10);", 20]
+    ].each do |input, expected|
+      test_integer(input, expected)
+    end
+  end
+
+  it 'error handling' do
+    [
+      ['5 + true;', 'type mismatch: Objects::MInteger + Objects::MBoolean'],
+      ['5 + true; 5;', 'type mismatch: Objects::MInteger + Objects::MBoolean'],
+      ['-true', 'unknown operator: -Objects::MBoolean'],
+      ['true + false;', 'unknown operator: Objects::MBoolean + Objects::MBoolean'],
+      [
+        'true + false + true + false;',
+        'unknown operator: Objects::MBoolean + Objects::MBoolean'
+      ],
+      [
+        '5; true + false; 5',
+        'unknown operator: Objects::MBoolean + Objects::MBoolean'
+      ],
+      [
+        'if (10 > 1) { true + false; }',
+        'unknown operator: Objects::MBoolean + Objects::MBoolean'
+      ],
+      [
+        "
+            if (10 > 1) {
+              if (10 > 1) {
+                return true + false;
+              }
+
+              return 1;
+            }
+            ",
+        'unknown operator: Objects::MBoolean + Objects::MBoolean'
+      ],
+      [
+        'foobar',
+        'identifier not found: foobar'
+      ],
+      [
+        %("Hello" - "World"),
+        'unknown operator: Objects::MString - Objects::MString'
+      ],
+      [
+        %({"name": "Monkey"}[fn(x) {x}];),
+        'unusable as a hash key: Objects::MFunction'
+      ]
+    ].each do |input, expected|
+      error = test_eval(input)
+      assert_equal expected, error.message
+    end
+  end
+
+  it 'let statement' do
+    [
+      ['let a = 5; a;', 5],
+      ['let a = 5 * 5; a;', 25],
+      ['let a = 5; let b = a; b;', 5],
+      ['let a = 5; let b = a; let c = a + b + 5; c;', 15]
+    ].each do |input, expected|
+      test_integer(input, expected)
+    end
+  end
+
+  it 'function object' do
+    input = 'fn(x) { x + 2; };'
+    fn = test_eval(input)
+    parameters = fn.parameters
+    assert_equal 1, parameters.size
+    assert_equal 'x', parameters[0].to_s
+    assert_equal '(x + 2)', fn.body.to_s
+  end
+
+  it 'function application' do
+    [
+      ['let identity = fn(x) { x; }; identity(5);', 5],
+      ['let identity = fn(x) { return x; }; identity(5);', 5],
+      ['let double = fn(x) { x * 2; }; double(5);', 10],
+      ['let add = fn(x, y) { x + y; }; add(5, 5);', 10],
+      ['let add = fn(x, y) { x + y; }; add(5 + 5, add(5, 5));', 20],
+      ['fn(x) { x; }(5)', 5]
+    ].each do |input, expected|
+      test_integer(input, expected)
+    end
+  end
+
+  it 'enclosing environments' do
+    input = "let first = 10;
+          let second = 10;
+          let third = 10;
+
+          let ourFunction = fn(first) {
+            let second = 20;
+
+            first + second + third;
+          };
+
+          ourFunction(20) + first + second;"
+    test_integer_object(test_eval(input), 70)
+  end
+
+  it 'string literal' do
+    test_string(%("Hello World!"), 'Hello World!')
+  end
+
+  it 'string concatenation' do
+    test_string(%("Hello" + " " + "World!"), 'Hello World!')
+  end
+
+  it 'builtin functions' do
+    [
+      [%(len("")), 0],
+      [%(len("four")), 4],
+      [%(len("hello world")), 11],
+      ['len(1)', 'argument to `len` not supported, got Objects::MInteger'],
+      [%(len("one", "two")), 'wrong number of arguments. got=2, want=1'],
+      ['len([1, 2, 3])', 3],
+      ['len([])', 0],
+      ['push([], 1)', [1]],
+      ['push(1, 1)', 'argument to `push` must be ARRAY, got Objects::MInteger'],
+      ['first([1, 2, 3])', 1],
+      ['first([])', nil],
+      ['first(1)', 'argument to `first` must be ARRAY, got Objects::MInteger'],
+      ['last([1, 2, 3])', 3],
+      ['last([])', nil],
+      ['last(1)', 'argument to `last` must be ARRAY, got Objects::MInteger'],
+      ['rest([1, 2, 3])', [2, 3]],
+      ['rest([])', nil]
+    ].each do |input, expected|
+      evaluated = test_eval(input)
+      case expected
+      when NilClass
+        test_nil_object(evaluated)
+      when Integer
+        test_integer_object(evaluated, expected)
+      when String
+        assert_equal expected, evaluated.message
+      when Array
+        assert_equal expected.size, evaluated.elements.size
+        expected.each_with_index do |element, i|
+          test_integer_object(evaluated.elements[i], element)
+        end
+      end
+    end
+  end
+
+  it 'array literal' do
+    result = test_eval('[1, 2 * 2, 3 + 3]')
+    assert_equal 3, result.elements.size
+    [1, 4, 6].each_with_index do |v, i|
+      test_integer_object(result.elements[i], v)
+    end
+  end
+
+  it 'array index expression' do
+    [
+      [
+        '[1, 2, 3][0]',
+        1
+      ],
+      [
+        '[1, 2, 3][1]',
+        2
+      ],
+      [
+        '[1, 2, 3][2]',
+        3
+      ],
+      [
+        'let i = 0; [1][i];',
+        1
+      ],
+      [
+        '[1, 2, 3][1 + 1];',
+        3
+      ],
+      [
+        'let myArray = [1, 2, 3]; myArray[2];',
+        3
+      ],
+      [
+        'let myArray = [1, 2, 3]; myArray[0] + myArray[1] + myArray[2];',
+        6
+      ],
+      [
+        'let myArray = [1, 2, 3]; let i = myArray[0]; myArray[i]',
+        2
+      ],
+      [
+        '[1, 2, 3][3]',
+        nil
+      ],
+      [
+        '[1, 2, 3][-1]',
+        nil
+      ]
+    ].each do |input, expected|
+      evaluated = test_eval(input)
+      case expected
+      when Integer
+        test_integer_object(evaluated, expected)
+      else
+        test_nil_object(evaluated)
+      end
+    end
+  end
+
+  it 'hash literals' do
+    input = %(
+      let two = "two";
+      	{
+      		"one": 10 - 9,
+      		two: 1 + 1,
+      		"thr" + "ee": 6 / 2,
+      		4: 4,
+      		true: 5,
+      		false: 6
+      	})
+    result = test_eval(input)
+    expected = {
+      Objects::MString.new('one').hash_key => 1,
+      Objects::MString.new('two').hash_key => 2,
+      Objects::MString.new('three').hash_key => 3,
+      Objects::MInteger.new(4).hash_key => 4,
+      Evaluator::M_TRUE.hash_key => 5,
+      Evaluator::M_FALSE.hash_key => 6
+    }
+    assert_equal 6, expected.size
+    assert_equal result.pairs.size, expected.size
+    expected.each do |expected_key, expected_value|
+      pair = result.pairs[expected_key]
+      assert !pair.nil?
+      test_integer_object(pair.value, expected_value)
+    end
+  end
+
+  it 'hash index expressions' do
+    [
+      [%({"foo": 5, "bar": 7}["foo"]), 5],
+      [%({"foo": 5}["bar"]), nil],
+      [%(let key = "foo";{"foo": 5}[key]), 5],
+      [%({}["foo"]), nil],
+      ['{5:5}[5]', 5],
+      ['{true:5}[true]', 5],
+      ['{false:5}[false]', 5]
+    ].each do |input, expected|
+      evaluated = test_eval(input)
+      case expected
+      when Integer
+        test_integer_object(evaluated, expected)
+      else
+        test_nil_object(evaluated)
+      end
+    end
+  end
+
+  it 'recursive fibonacci' do
+    input = "
+let fibonacci = fn(x) {
+	if (x == 0) {
+		return 0;
+	} else {
+		if (x == 1) {
+			return 1;
+		} else {
+			fibonacci(x - 1) + fibonacci(x - 2);
+		}
+	}
+};
+fibonacci(15);"
+    evaluated = test_eval(input)
+    test_integer_object(evaluated, 610)
+  end
+end
